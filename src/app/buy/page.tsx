ea5d55f61c0fea1f248cdfe4046c60aec3ffcf6e
Raju Kumar <rajuk76427@gmail.com>
2026-01-28 15:17:19 +0000

    100_- 5order,200-6,300-7,400-5,500-6,600-5,700-4,800-3,1000-4,2000,3,300

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -35,17 +35,26 @@ import { addDoc, collection, serverTimestamp, query, where } from 'firebase/fire
 import { useToast } from '@/hooks/use-toast';
 
 const smallPurchaseOptions = [
-  { id: 1, amount: 500 },
-  { id: 2, amount: 1000 },
-  { id: 3, amount: 3000 },
-  { id: 4, amount: 5000 },
+  { id: 1, amount: 100 },
+  { id: 2, amount: 200 },
+  { id: 3, amount: 300 },
+  { id: 4, amount: 400 },
+  { id: 5, amount: 500 },
+  { id: 6, amount: 600 },
+  { id: 7, amount: 700 },
+  { id: 8, amount: 800 },
+  { id: 9, amount: 1000 },
 ];
 
 const highPurchaseOptions = [
-  { id: 8, amount: 100000 },
-  { id: 7, amount: 50000 },
-  { id: 6, amount: 20000 },
-  { id: 5, amount: 10000 },
+  { id: 10, amount: 2000 },
+  { id: 11, amount: 3000 },
+  { id: 12, amount: 4000 },
+  { id: 13, amount: 5000 },
+  { id: 14, amount: 6000 },
+  { id: 15, amount: 7000 },
+  { id: 16, amount: 8000 },
+  { id: 17, amount: 10000 },
 ];
 
 const upiMethods = [
