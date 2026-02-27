52e4e6c044a6f930504eed5c1a34e3f6efec591c
Raju Kumar <rajuk76427@gmail.com>
2026-02-27 17:44:04 +0000

    Ab yha pe jaise ar wallet ke invite link me app icon aa rha he usi type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,19 @@ import { LanguageProvider } from '@/context/language-context';
 
 export const metadata: Metadata = {
   title: 'LG Pay',
-  description: 'Secure and easy payments with LG Pay',
+  description: 'Secure and easy payments with LG Pay. Join now and earn rewards!',
   icons: {
     icon: 'https://firebasestorage.googleapis.com/v0/b/studio-7631087921-85112.firebasestorage.app/o/file_000000002968720686f855daed13e880.png?alt=media&token=c4dece97-7dee-41c4-bac7-6c1f9f186fb6',
+  },
+  openGraph: {
+    title: 'Join LG Pay and get exclusive rewards!',
+    description: 'Use my link to sign up for LG Pay for secure payments and start earning today.',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/studio-7631087921-85112.firebasestorage.app/o/file_000000002968720686f855daed13e880.png?alt=media&token=c4dece97-7dee-41c4-bac7-6c1f9f186fb6',
+      },
+    ],
+    type: 'website',
   },
 };
 
