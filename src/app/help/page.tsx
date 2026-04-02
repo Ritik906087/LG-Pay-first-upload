bc20a1ecfbad2c39ff1a562495ee18de0f391da5
Raju Kumar <rajuk76427@gmail.com>
2026-04-02 05:49:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/help/page.tsx
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank to resolve the route conflict.
-// The correct help page is located at src/app/(auth)/help/page.tsx.
-// By not exporting a default React component, this file will not create a page route.
