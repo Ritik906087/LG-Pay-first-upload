a61767aac988008d852246cf22527003a21df043
Raju Kumar <rajuk76427@gmail.com>
2026-04-05 14:03:02 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    AlertDialogTrigge

--- src/app/buy/confirm/[orderId]/page.tsx
+++ src/app/buy/confirm/[orderId]/page.tsx
@@ -30,6 +30,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Textarea } from '@/components/ui/textarea';
