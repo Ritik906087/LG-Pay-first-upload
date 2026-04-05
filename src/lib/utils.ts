a5b6d35c3fa94431f8c091d8ab28c08982a90481
Raju Kumar <rajuk76427@gmail.com>
2026-04-05 10:48:07 +0000

    Buy me order create hone ke baad bhi ye problem

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { createBrowserClient } from '@supabase/ssr'
+import type { SupabaseClient } from '@supabase/supabase-js'
+
+let supabase: SupabaseClient | undefined;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -19,8 +22,12 @@ export function getURL() {
 };
 
 export function createClient() {
-  return createBrowserClient(
+  if (supabase) {
+    return supabase;
+  }
+  supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+  );
+  return supabase;
 }
